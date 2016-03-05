65494e2cae639e1b6826cf5c37cd82b668ceac0d
Oscar Caballero <oscarcpozas@gmail.com>
2016-03-05 13:45:27 +0100

    Merge branch 'master' of https://github.com/oscarcpozas/Weather
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/study/jam/weather/service/WeatherService.java
    #	app/src/main/java/com/study/jam/weather/ui/activity/MainActivity.java

--- app/src/main/java/com/study/jam/weather/ui/activity/MainActivity.java
+++ app/src/main/java/com/study/jam/weather/ui/activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.study.jam.weather.ui.activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
